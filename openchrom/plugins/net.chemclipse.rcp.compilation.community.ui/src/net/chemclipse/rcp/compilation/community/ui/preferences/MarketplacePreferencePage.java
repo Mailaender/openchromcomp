74fba4acb10c984cc7be736473d9ca55a6f921db
Dr. Philip Wenig <philip.wenig@openchrom.net>
2014-02-25 11:24:45 +0100

    OpenChrom has been prepared for the ChemClipse/OpenChrom simultaneous release. The open source plug-ins have been renamed to chemclipse.

--- openchrom/plugins/net.chemclipse.rcp.compilation.community.ui/src/net/chemclipse/rcp/compilation/community/ui/preferences/MarketplacePreferencePage.java
+++ openchrom/plugins/net.chemclipse.rcp.compilation.community.ui/src/net/chemclipse/rcp/compilation/community/ui/preferences/MarketplacePreferencePage.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2014 Philip (eselmeister) Wenig.
+ * Copyright (c) 2012, 2014 Dr. Philip Wenig.
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * 
  * Contributors:
- * openchrom - initial API and implementation
+ * Dr. Philip Wenig - initial API and implementation
  *******************************************************************************/
 package net.chemclipse.rcp.compilation.community.ui.preferences;
 
@@ -27,9 +27,9 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 import net.chemclipse.rcp.compilation.community.ui.Activator;
-import net.openchrom.logging.core.Logger;
-import net.openchrom.support.ui.preferences.fieldeditors.LabelFieldEditor;
-import net.openchrom.support.ui.preferences.fieldeditors.SpacerFieldEditor;
+import net.chemclipse.logging.core.Logger;
+import net.chemclipse.support.ui.preferences.fieldeditors.LabelFieldEditor;
+import net.chemclipse.support.ui.preferences.fieldeditors.SpacerFieldEditor;
 
 public class MarketplacePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
@@ -39,7 +39,7 @@ public class MarketplacePreferencePage extends FieldEditorPreferencePage impleme
 
 		super(GRID);
 		// The preference store is not needed.
-		setDescription("Add the OpenChrom Marketplace on demand, to fetch additional plug-ins. It is still included in the OpenChrom release. Hence, it's only needed when using the OpenChrom plug-ins in a regular Eclipse IDE.");
+		setDescription("Add the marketplace on demand, to fetch additional plug-ins.");
 	}
 
 	@Override
@@ -51,7 +51,7 @@ public class MarketplacePreferencePage extends FieldEditorPreferencePage impleme
 	protected void createFieldEditors() {
 
 		addField(new SpacerFieldEditor(getFieldEditorParent()));
-		addField(new LabelFieldEditor("Note: The marketplace is still included in the regular OpenChrom release.", getFieldEditorParent()));
+		addField(new LabelFieldEditor("Note: The marketplace is already included in the regular release.", getFieldEditorParent()));
 		/*
 		 * Add additional update sites.
 		 */
@@ -67,12 +67,12 @@ public class MarketplacePreferencePage extends FieldEditorPreferencePage impleme
 				 */
 				MessageBox messageBox = new MessageBox(Display.getCurrent().getActiveShell(), SWT.ICON_INFORMATION | SWT.YES | SWT.NO | SWT.CANCEL);
 				messageBox.setText("Add OpenChrom Marketplace");
-				messageBox.setMessage("Do you really want to add the OpenChrom marketplace?");
+				messageBox.setMessage("Do you really want to add the marketplace?");
 				if(messageBox.open() == SWT.YES) {
 					try {
 						URL url = new URL("http://openchrom.dyndns.org/");
 						CatalogDescriptor catalogDescriptor = new CatalogDescriptor(url, "OpenChrom Marketplace");
-						catalogDescriptor.setDescription("Install new plug-ins from the OpenChrom marketplace.");
+						catalogDescriptor.setDescription("Install new plug-ins from the marketplace.");
 						catalogDescriptor.setIcon(Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/logo_32x32.png"));
 						catalogDescriptor.setInstallFromAllRepositories(false);
 						MarketplaceClient.addCatalogDescriptor(catalogDescriptor);
